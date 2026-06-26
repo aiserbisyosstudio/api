038cd29e70a1abf75808c61990bc79c3b2b5487c
AISerbisyosStudio Admin <aiserbisyosstudio@gmail.com>
2026-06-26 11:19:07 +0530

    Changes for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import app from "../src/app.js";
+import connectDB from "../src/config/database.js";
+
+let isConnected = false;
+
+const handler = async (req, res) => {
+  try {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error("Server Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+};
+
+export default handler;
